146d236d377c3f93fd33f395c6545466ae643b0b
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2023-07-06 15:52:07 +0900

    adjust vs path.

--- Project/Nuget/MakeNupack.bat
+++ Project/Nuget/MakeNupack.bat
@@ -1,3 +1,3 @@
-"%DevEnvDir%devenv.exe" "../Friendly.C1.Win.sln" /rebuild Release
-"%DevEnvDir%devenv.exe" "../Friendly.C1.Win.sln" /rebuild Release-ENG
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe" "../Friendly.C1.Win.sln" /rebuild Release
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe" "../Friendly.C1.Win.sln" /rebuild Release-ENG
 nuget pack Friendly.C1.Win.nuspec
